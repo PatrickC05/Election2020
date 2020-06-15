e876e34f3e437494f8b680abc11577a772085e83
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2020-06-15 10:29:12 -0400

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -1,18 +1,72 @@
 import csv
 import numpy as np
+import math
 
-class State():
+abbreviations = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
+class State(object):
     """
     A state contains critical information about a state
     to determine its preference
     
     There are others things to consider as well, but this is a really important part
     """
-    def __init__(self, name, votes = None, polls=[], approval=[]):
+    def __init__(self, name, votes = None, polls=[]):
         self.name = name
         self.votes = votes
         self.polls = polls #List of polls, each element is poll data
-        self.approval = approval #List of approval ratings, each element is poll data
         
     def __str__(self):
         return self.name
@@ -29,6 +83,41 @@ class State():
     def addPoll(self, poll):
         self.polls.append(poll)
         
-    def addApproval(self, approval):
-        self.approval.append(approval)
 
+class Poll(object):
+    """
+    A poll gives a rough estimate of how the electorate in a state is thinking
+    """
+    def __init__(self, pollster, start, end, state = None, sample = 0,
+                 method = None, Dpct = 0, Rpct = 0):
+        self.pollster = pollster
+        self.start = start
+        self.end = end
+        self.state = state
+        self.sample = sample
+        self.N = method
+        self.Dpct = Dpct
+        self.Rpct = Rpct
+        self.Rmargin = Rpct - Dpct
+        s = Dpct + Rpct
+        self.Dpct = round(Dpct*100.0/s, 2)
+        self.Rpct = round(Rpct*100.0/s, 2)
+        
+        
+    def __str__(self):
+        if self.margin > 0:
+            m = "Trump +" + str(self.margin)
+        elif self.margin < 0:
+            m = "Biden +" + str(-self.margin)
+        else:
+            m = "TIE"
+        return self.name + " Date: " + self.start +"-" + self.end + " Result: " + m
+    
+    def getState(self):
+        return self.state
+    
+    def getMethod(self):
+        return self.method
+    
+    def getMargin(self):
+        return self.Rmargin
